7fe8203245835eadfa34ac576333e84cbc4fd445
Alan <alancurtis5000+accounts@gmail.com>
2022-02-05 00:57:19 -0700

    adding test utils overide

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from './tests/test.utils';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const sidebarTitle = screen.getByText(/Projects/i);
+  expect(sidebarTitle).toBeInTheDocument();
 });
---
+++ src/tests/test.utils.js
@@ -0,0 +1,35 @@
+// test-utils.js
+// this is a custom render for components connected to redux
+// also connects to react-router-dom
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import PropTypes from 'prop-types';
+// import configureAppStore from '../redux/configure-store';
+// import { Provider } from 'react-redux';
+import { BrowserRouter, Router } from 'react-router-dom';
+
+function render(
+  ui,
+  {
+    // preloadedState,
+    // store = configureAppStore(preloadedState),
+    ...renderOptions
+  } = {}
+) {
+  const Wrapper = ({ children }) => {
+    return (
+      // <Provider store={store}>
+      <BrowserRouter>{children}</BrowserRouter>
+      // </Provider>
+    );
+  };
+  Wrapper.propTypes = {
+    children: PropTypes.node,
+  };
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render };
