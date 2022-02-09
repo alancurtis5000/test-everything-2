9251eb3d814faeb57445911e1c02d64660ec289b
Alan <alancurtis5000+accounts@gmail.com>
2022-02-08 22:33:38 -0700

    test for reset and minus

---
+++ src/projects/context-examples/features/counter/minus-button.component.test.js
@@ -0,0 +1,48 @@
+// We're using our own custom render function for redux conneceted components and not RTL's render
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
+import MinusButton from './minus-button.component';
+import { CounterContext } from '../../providers/counter.provider';
+
+describe('MinusButton', () => {
+  afterEach(cleanup);
+
+  describe('Default State', () => {
+    let contextState = {
+      count: 0,
+      margin: 1,
+      decrement: jest.fn(),
+    };
+    beforeEach(() => {
+      render(
+        <CounterContext.Provider value={contextState}>
+          <MinusButton />
+        </CounterContext.Provider>
+      );
+    });
+    it('should show "Minus" text', () => {
+      expect(screen.getByText('Minus')).toBeDefined();
+    });
+
+    it('should click "Minus" button', () => {
+      fireEvent.click(screen.getByText('Minus'));
+      expect(contextState.decrement).toHaveBeenCalled();
+    });
+  });
+
+  describe('Altered State', () => {
+    let contextState = {
+      count: 0,
+      margin: 0,
+    };
+    beforeEach(() => {
+      render(
+        <CounterContext.Provider value={contextState}>
+          <MinusButton />
+        </CounterContext.Provider>
+      );
+    });
+    it('should show disabled "Minus" Button', () => {
+      expect(screen.getByTestId('minus')).toBeDisabled();
+    });
+  });
+});
---
+++ src/projects/context-examples/features/counter/reset-button.component.test.js
@@ -0,0 +1,31 @@
+// We're using our own custom render function for redux conneceted components and not RTL's render
+import { render, cleanup, screen, fireEvent } from '@testing-library/react';
+import ResetButton from './reset-button.component';
+import { CounterContext } from '../../providers/counter.provider';
+
+describe('ResetButton', () => {
+  afterEach(cleanup);
+
+  describe('Default State', () => {
+    let contextState = {
+      count: 0,
+      margin: 1,
+      reset: jest.fn(),
+    };
+    beforeEach(() => {
+      render(
+        <CounterContext.Provider value={contextState}>
+          <ResetButton />
+        </CounterContext.Provider>
+      );
+    });
+    it('should show "Reset" text', () => {
+      expect(screen.getByText('Reset')).toBeDefined();
+    });
+
+    it('should click "Reset" button', () => {
+      fireEvent.click(screen.getByText('Reset'));
+      expect(contextState.reset).toHaveBeenCalled();
+    });
+  });
+});
